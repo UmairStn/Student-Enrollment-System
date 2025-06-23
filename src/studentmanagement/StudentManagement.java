d1cdd4cb09a3b1cb170e91d86f4f21b8de749c93
UmairStn <umrstn28@gmail.com>
2025-06-23 19:27:00 +0530

    First Commit

---
+++ src/studentmanagement/StudentManagement.java
@@ -0,0 +1,61 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package studentmanagement;
+
+/**
+ *
+ * @author umrstn
+ */
+public class StudentManagement {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        StudentManagement.Home();
+    }
+    
+    
+    // ADMIN
+    //login page visible
+    public static void Home(){
+        Login loginForm = new Login();
+        loginForm.setVisible(true);
+    }
+    
+    //Admi_Login visible
+    public static void AdminLoginAccess(){
+        Admin_Login adminForm = new Admin_Login();
+        adminForm.setVisible(true);
+    }
+    
+    //Admin dashboard visible
+    public static void AdminDashboard(){
+        Admin_Dashboard adminDashboard = new Admin_Dashboard();
+        adminDashboard.setVisible(true);
+    }
+    
+    //add student page
+    public static void AddStudent(){
+        Add_Student addStudent = new Add_Student();
+        addStudent.setVisible(true);
+    }
+    
+    //View student details
+    public static void ViewStudent(){
+        Student_Details viewStudent = new Student_Details();
+        viewStudent.setVisible(true);
+    }
+    
+    //Add student page for students
+    public static void AddstudentStd(){
+        Add_studentStd addStudentStd = new Add_studentStd();
+        addStudentStd.setVisible(true);
+    }
+    
+    
+    
+}
